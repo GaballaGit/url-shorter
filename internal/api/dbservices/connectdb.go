ead93a3bf0109d4588743cd63f6acc3b386b6968
Gaballa <lGaballal@yahoo.com>
2026-06-15 13:44:04 -0700

    pass logger into conn db

--- internal/api/dbservices/connectdb.go
+++ internal/api/dbservices/connectdb.go
@@ -8,11 +8,11 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func ConnectDB(ctx context.Context) *pgx.Conn {
+func ConnectDB(ctx context.Context, logger slog.Logger) *pgx.Conn {
 
 	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
-		slog.Error("error opening database:" + err.Error())
+		logger.Error("error opening database:" + err.Error())
 		os.Exit(1)
 	}
 	defer conn.Close(ctx)
